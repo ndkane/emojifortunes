666f97d7895c7608f61d1e7176a064c7b1ceb4f2
ndkane <3464509+ndkane@users.noreply.github.com>
2017-12-01 17:58:15 +0000

    Add phrase

--- bot.js
+++ bot.js
@@ -9,13 +9,14 @@ var Bot = new TwitterBot({
 var phraseArray = ["Tweet me your last ten emojis to see into your future...",
     "10 emojis is all I need...",
     "Cross my palm with emojis to see into the future...",
-    "Cross my palm with emojis to see what you need to do..."
+    "Cross my palm with emojis to see what you need to do...",
+    "Emoji, emoji, what do they mean?"
 ];
 
 function chooseRandom(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
-var phrase = chooseRandom(phraseArray) + ", Friend.";
+var phrase = chooseRandom(phraseArray);
 Bot.tweet(phrase);
 
 setInterval(function() {
