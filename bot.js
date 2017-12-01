0fd78afa133f334a7f5809227073ebb5845cb1ab
ndkane <3464509+ndkane@users.noreply.github.com>
2017-12-01 18:18:36 +0000

    new functions and setInterval added

--- bot.js
+++ bot.js
@@ -6,23 +6,23 @@ var Bot = new TwitterBot({
     access_token: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = ["Tweet me your last ten emojis to see into your future...",
-    "10 emojis is all I need...",
-    "Cross my palm with emojis to see into the future...",
-    "Cross my palm with emojis to see what you need to do...",
-    "Emoji, emoji, what do they mean?"
-];
 
-function chooseRandom(myArray) {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-var phrase = chooseRandom(phraseArray);
-Bot.tweet(phrase);
+tweetIt();
+setInterval(tweetIt, 1000*60*5);
 
-setInterval(function() {
-    try {
-        run();
-    } catch (e) {
-        console.log(e);
+function tweetIt(){
+
+    var phraseArray = ["Tweet me your last ten emojis to see into your future...",
+        "10 emojis is all I need...",
+        "Cross my palm with emojis to see into the future...",
+        "Cross my palm with emojis to see what you need to do...",
+        "Emoji, emoji, what do they mean?"
+    ];
+
+    function chooseRandom(myArray) {
+        return myArray[Math.floor(Math.random() * myArray.length)];
     }
-}, 5000 * 60);
+    var phrase = chooseRandom(phraseArray);
+    Bot.tweet(phrase);
+}
+
