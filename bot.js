fbad9055ed45b7bb3b2b46a6230221ffc9df7318
ndkane <3464509+ndkane@users.noreply.github.com>
2017-12-01 17:56:35 +0000

    remove event reply

--- bot.js
+++ bot.js
@@ -25,36 +25,3 @@ setInterval(function() {
         console.log(e);
     }
 }, 5000 * 60);
-
-// reply whenever another account tweets to this account
-var stream = T.stream('user');
-stream.on('tweet', tweetEvent);
-
-function tweetEvent(eventMsg) {
-
-    var replyTo = eventMsg.in_reply_to_screen_name;
-    var text = eventMsg.text;
-    var from = eventMsg.user.screen_name;
-    console.log('tweet received from another account: ', text);
-
-
-    if (replyTo == 'dpf205') {
-        var newTweet = '\n this is a response to a tweet from ' + '@' + from;
-        replyTweet(newTweet);
-    }
-}
-
-function replyTweet(tweetText) {
-    var post_params = {
-        status: tweetText
-    };
-
-    T.post('statuses/update', post_params, logTweet);
-    function logTweet(err, data, response) {
-        if (err) {
-            return err;
-        } else {
-            console.log('\n tweet sent from replier bot: ', post_params.status);
-        }
-    }
-}
