26572e926d9163b7d5c2c6c6cd3d5fb9f37c60b9
Stephan Hügel <urschrei@gmail.com>
2017-12-01 17:08:40 +0000

    Update bot.js
    The major problem was that you were using curly quotes (`‘`) instead of straight quotes (you can use single or double: `'` or `"` quotes). Dunno if this will fix it, but it's a first step…

--- bot.js
+++ bot.js
@@ -1,25 +1,26 @@
-var Twit = require(‘twit’);
-var TwitterBot = require(‘node-twitterbot’).TwitterBot;
+var Twit = require("twit");
+var TwitterBot = require("node-twitterbot").TwitterBot;
 var Bot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = [ "Tweet me your last ten emojis to see into your future...",
-                    "10 emojis is all I need...",
-                    "Cross my palm with emojis to see into the future..." ];
+var phraseArray = ["Tweet me your last ten emojis to see into your future...",
+    "10 emojis is all I need...",
+    "Cross my palm with emojis to see into the future..."
+];
+
 function chooseRandom(myArray) {
-  return myArray[Math.floor(Math.random() * myArray.length)];
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
 Bot.tweet(phrase);
 
 setInterval(function() {
-  try {
-    run();
-  }
-  catch (e) {
-    console.log(e);
-  }
-}, 5000* 60);
+    try {
+        run();
+    } catch (e) {
+        console.log(e);
+    }
+}, 5000 * 60);
